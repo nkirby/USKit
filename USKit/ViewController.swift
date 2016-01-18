68b8c054c3bb0e0eddf98f7393c0d49b98f832ae
Nathaniel Kirby <nate@projectspong.com>
2016-01-18 10:41:03 -0500

    Initial Commit

---
+++ USKit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  USKit
+//
+//  Created by Nate Kirby on 1/18/16.
+//  Copyright © 2016 Nate Kirby. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
