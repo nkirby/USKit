22da6b58b225b26681358c541c3782bae3658454
Nate Kirby <nkirby.ps@gmail.com>
2016-01-18 14:00:39 -0500

    First pass UIFont

---
+++ USKit/lib/Font/UIFont.swift
@@ -0,0 +1,35 @@
+// =======================================================
+// USKit: A shared platform for Cocoa applications
+// github.com/nkirby/USKit
+// =======================================================
+
+#if os(OSX)
+import Cocoa
+    
+public typealias UIFont = NSFont
+
+public extension UIFont {
+    public class func familyNames() -> [String] {
+        USUnimplemented()
+    }
+    
+    public class func fontNamesForFamilyName(familyName: String) -> [String] {
+        USUnimplemented()
+    }
+    
+    public class func italicSystemFontOfSize(size: CGFloat) -> UIFont {
+        USUnimplemented()
+    }
+    
+    public func fontWithSize(fontSize: CGFloat) -> UIFont {
+        return UIFont(name: self.fontName, size: fontSize)!
+    }
+    
+    // Because we can't add a instance variable in an extension. We may need to drop down
+    // to core text here.
+    public func lineHeight() -> CGFloat {
+        return -1.0
+    }
+}
+
+#endif
---
+++ USKit/lib/Geometry/UIOffset.swift
@@ -0,0 +1,13 @@
+//
+//  UIOffset.swift
+//  USKit
+//
+//  Created by Nate Kirby on 1/18/16.
+//  Copyright © 2016 Nate Kirby. All rights reserved.
+//
+
+import Cocoa
+
+class UIOffset: NSObject {
+
+}
